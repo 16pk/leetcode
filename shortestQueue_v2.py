18192cff7159e51f1e1d67c4b5696c7cb9f12a20
hongjian.liu <hongjian.liu@envisioncn.com>
2018-05-07 19:49:15 +0800

    add shortestQueue

--- shortestQueue_v2.py
+++ shortestQueue_v2.py
@@ -13,34 +13,37 @@ import time
 
 class Solution(object):
     def findLadders(self, start_word, end_word, word_dict):
-        if end_word not in word_dict:
+        word_set = set(word_list)
+        if end_word not in word_set:
             return []
         used_word_set = [{start_word}, {end_word}]
         half_results = [[[start_word]], [[end_word]]]
-        head_tail = 0
+        flop = 0
+        parents = collections.defaultdict(list)
         while not bool(used_word_set[0] & used_word_set[1]):
             meta_set = set()
             meta_dict = {}
             new_que_list = []
-            for crt_que in half_results[head_tail]:
+            for crt_que in half_results[flop]:
                 tgt_word = crt_que[-1]
-                if tgt_word in meta_dict:
-                    for new_word in meta_dict[tgt_word]:
+                if tgt_word in parents:
+                    for new_word in parents[tgt_word]:
                         new_que_list.append(crt_que + [new_word])
                 else:
-                    meta_dict[tgt_word] = []
-                    for new_word in word_dict:
-                        if new_word in used_word_set[head_tail]:
-                            continue
-                        if self.is_similar(new_word, tgt_word):
-                            meta_set.add(new_word)
-                            new_que_list.append(crt_que + [new_word])
-                            meta_dict[tgt_word].append(new_word)
-            used_word_set[head_tail].update(meta_set)
+                    for ii in range(len(tgt_word)):
+                        for pp in 'abcdefghijklmnopqrstuvwxyz':
+                            new_word = tgt_word[:ii] + pp + tgt_word[ii + 1:]
+                            if (pp == tgt_word[ii]) or (new_word in used_word_set[flop]):
+                                continue
+                            if new_word in word_set:
+                                parents[tgt_word].append(new_word)
+                                meta_set.add(new_word)
+                                new_que_list.append(crt_que + [new_word])
+            used_word_set[flop].update(meta_set)
             if not bool(new_que_list):
                 return []
-            half_results[head_tail] = new_que_list
-            head_tail = (head_tail + 1) % 2
+            half_results[flop] = new_que_list
+            flop = (flop + 1) % 2
         connected_word_set = used_word_set[0] & used_word_set[1]
         result = []
         for connected_word in connected_word_set:
@@ -57,19 +60,6 @@ class Solution(object):
             if queue_list[idx][-1] == word:
                 target.append(idx)
         return target
-
-    def is_similar(self, word1, word2):
-        idx, n_diff = 0, 0
-        if word1 == word2:
-            return False
-        while (n_diff <= 1) and (idx < len(word1)):
-            if word1[idx] != word2[idx]:
-                n_diff += 1
-            idx += 1
-        if n_diff == 1:
-            return True
-        else:
-            return False
 
 
 if __name__ == '__main__':
